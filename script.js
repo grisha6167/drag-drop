ba91b3ae122da02ad47f37acb65d60464cb1fb8d
grisha6167 <grisha6167@gmail.com>
2023-03-28 13:06:24 +0700

    finish

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-const placeholders = document.querySelectorAll(".placeholder") 
+const placeholders = document.querySelectorAll(".placeholder")
 const item = document.querySelector(".item")
 
 item.addEventListener("dragstart", dragstart)
 item.addEventListener("dragend", dragend)
 
-for(placeholder of placeholders){
+for (placeholder of placeholders) {
     placeholder.addEventListener("dragover", dragover)
     placeholder.addEventListener("dragenter", dragenter)
     placeholder.addEventListener("dragleave", dragleave)
     placeholder.addEventListener("drop", dragdrop)
 }
 
-function dragstart(event){
+function dragstart(event) {
     event.target.classList.add("hold")
+    setTimeout(() => event.target.classList.add("hide"), 0)
 }
-function dragend(event){
+function dragend(event) {
     event.target.className = "item"
 }
-function dragover(event){
+function dragover(event) {
     event.preventDefault()
 }
-function dragenter(event){
- event.target.classList.add("hovered")   
+function dragenter(event) {
+    event.target.classList.add("hovered")
 }
-function dragleave(event){
+function dragleave(event) {
     event.target.classList.remove("hovered")
 }
-function dragdrop(event){
+function dragdrop(event) {
     event.target.classList.remove("hovered")
     event.target.append(item)
+}
+let i = 0;
+
+setTimeout(() => alert(i), 100); // ?
+
+// предположим, что время выполнения этой функции >100 мс
+for(let j = 0; j < 100000000; j++) {
+  i++;
 }
